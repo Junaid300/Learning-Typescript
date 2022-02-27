615bf6f3169168a187d57bb86fa980e010ecd670
junaid <junaidklair8@gmail.com>
2022-02-27 13:23:50 +0500

    learning about types

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("HELLO WORLD");
+var age = 29;
+var brotherAGe = 20;
+function good(age1, age2) {
+    return age1 + age2;
+}
+good(age, brotherAGe);
---
+++ app.ts
@@ -0,0 +1,12 @@
+console.log("HELLO WORLD")
+
+
+let age:number = 29;
+let brotherAGe:number=20;
+
+
+function good(age1:number,age2:number) {
+    return age1+age2;
+}
+
+good(age,brotherAGe)
